cdb5cf5cf60fa04ee4f30bef34d43da678085a6c
miinnnnnnn <kiemtienkhong241@gmail.com>
2026-09-14 13:39:18 +0700

    fix: remove infinite reload loop in Main.js causing blank page

--- rent-manager-app/src/page/user/Main.js
+++ rent-manager-app/src/page/user/Main.js
@@ -3,14 +3,8 @@ import Header from "../../common/Header";
 import IntroCarosel from "../../common/IntroCarousel";
 import Home from "../../common/Home";
 import Footer from "../../common/Footer";
-import { useLocation } from 'react-router-dom';
 
 function Main(props) {
-    const location = useLocation();
-    
-    if (location.state) {
-      window.location.reload();
-    }
   return (
     <>
       <Header authenticated={props.authenticated} currentUser={props.currentUser} onLogout={props.onLogout}/>
